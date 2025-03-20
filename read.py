876d18c242ee32f47b326ec522dc1fc3d39c5d54
Eddie Chen <gerg2windy@gmail.com>
2025-03-20 20:12:06 +0800

    add word count

--- read.py
+++ read.py
@@ -1,14 +1,11 @@
-
 data = []
 count = 0
-
-with open('reviews.txt.crdownload', 'r') as f:
+with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
 		if count % 1000 ==0:
 			print(len(data))
-
 print('The file has been read, total has', len(data), 'sets data')
 
 sum_len = 0
@@ -34,3 +31,34 @@ print('Total has', len(good), 'comments mentioned about good')
 
 #for快寫法
 good = [d for d in data if 'good' in d]
+
+
+#文字計數
+wc = {} #word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 #新增新的key進wc字典
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+print(len(wc))
+print(wc['Eddie'])
+
+while True:
+	word = input('請問你想查什麼字： ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(f'{word}出現過的次數為{wc[word]}')
+	else:
+		print('這個字沒有出現過')
+
+
+print('感謝使用本查詢功能')
+
